d42723b528081a5f6d1c064c00fff452529e7a77
sivasspor <yusufulku58@hotmail.com>
2016-02-17 21:43:22 +0200

    Update weblinks.php

--- Turkish/infusions/weblinks/locale/Turkish/weblinks.php
+++ Turkish/infusions/weblinks/locale/Turkish/weblinks.php
@@ -1,9 +1,9 @@
 <?php
-$locale['400'] = "Web Links";
+$locale['400'] = "Web Linkleri";
 // Download File Information
-$locale['410'] = "New";
-$locale['411'] = "Date Added:";
-$locale['412'] = "Visits:";
+$locale['410'] = "Yeni";
+$locale['411'] = "Ekleme Tarihi:";
+$locale['412'] = "Ziyaretler:";
 // Downloads Notices
 $locale['430'] = "No Web Link Categories defined";
 $locale['431'] = "No Web Links have been added to this Category";
