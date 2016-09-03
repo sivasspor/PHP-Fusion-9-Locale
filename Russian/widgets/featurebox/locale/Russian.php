8691c1402c924a6c04e3cf86a720fd0943d51a40
Rizado <v.chubatyj@yandex.ru>
2016-09-03 17:19:38 +0300

    Comments updated. 4 of 6 widgets translated to Russian

---
+++ Russian/widgets/featurebox/locale/Russian.php
@@ -0,0 +1,53 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------*
+| Filename: Featurebox/locale/English.php
+| Author: Frederick MC Chan (Chan)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+
+$locale['0100'] = "Feature Box";
+$locale['0101'] = "Feature Box Plugin";
+$locale['0200'] = "Box Title";
+$locale['0201'] = "Box Description";
+$locale['0202'] = "Box Icon Type";
+$locale['0203'] = "CSS Icon";
+$locale['0204'] = "Image Icon";
+$locale['0205'] = "Please choose or upload your icon image";
+$locale['0206'] = "Please fill icon configurations";
+$locale['0207'] = "Icon class";
+$locale['0208'] = "Please refer to your svg icon code. i.e. Font-Awesome: \"fa fa-thumbs-up-o\"";
+$locale['0209'] = "Icon size";
+$locale['0210'] = "Pixels. Negative number not allowed.";
+$locale['0211'] = "Icon color";
+$locale['0212'] = "Icon stacked class";
+$locale['0213'] = "Icon stacked size";
+$locale['0214'] = "Icon stacked color";
+$locale['0215'] = "Link URL";
+$locale['0216'] = "Link class";
+$locale['0217'] = "Box Margins and Spacing";
+$locale['0218'] = "Icon Margin Top";
+$locale['0219'] = "Specify top margin between icon and the border";
+$locale['0220'] = "Icon Margin Bottom";
+$locale['0221'] = "Specify bottom margin between icon and the title";
+$locale['0222'] = "Box Padding";
+$locale['0223'] = "Specify distance of top, left, right, bottom of the feature box";
+$locale['0224'] = "Box Link Margin Top";
+$locale['0225'] = "Specify the top margin between link and the description";
+$locale['0226'] = "Box Link Margin Bottom";
+$locale['0227'] = "Specify the bottom margin between link and the border";
+$locale['0228'] = "Save Feature Box";
+$locale['0229'] = "Save and Close Feature Box";
+$locale['0230'] = "Box Class";
+
+$locale['0300'] = "Find out more";
---
+++ Russian/widgets/slider/locale/Russian.php
@@ -0,0 +1,68 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------*
+| Filename: Slider/locale/English.php
+| Author: Frederick MC Chan (Chan)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['0100'] = "Carousel";
+$locale['0101'] = "Slider Plugin";
+$locale['0200'] = "Slide Deleted";
+$locale['0300'] = "Current Slides";
+$locale['0301'] = "Edit Slide";
+$locale['0302'] = "Add Slide";
+$locale['0303'] = "Carousel Settings";
+$locale['0400'] = "Slide Title";
+$locale['0401'] = "Slide Image";
+$locale['0402'] = "Slide Order";
+$locale['0403'] = "Actions";
+$locale['0404'] = "There are no slides defined";
+$locale['0405'] = "Configure the properties of the slider";
+$locale['0406'] = "Attention: Please configure and save the properties of the slider before use.";
+$locale['0500'] = "Slider HTML ID";
+$locale['0501'] = "Slider Height";
+$locale['0502'] = "Slider Navigation";
+$locale['0503'] = "Do not display Navigation Control";
+$locale['0504'] = "Show and display Navigation Control";
+$locale['0505'] = "Slider Indicator";
+$locale['0506'] = "Slider Indicator Settings";
+$locale['0507'] = "Do not display Slider Indicator";
+$locale['0508'] = "Show and display Slider Indicator";
+$locale['0510'] = "Background Image";
+$locale['0511'] = "Choose or upload new background";
+$locale['0512'] = "Please select a valid background";
+$locale['0513'] = "Slider Heading Title";
+$locale['0514'] = "Slider Description";
+$locale['0515'] = "Link URL";
+$locale['0516'] = "Order";
+$locale['0517'] = "Slide Attributes";
+$locale['0518'] = "Configure appearance for this Slide";
+$locale['0519'] = "Caption Top Offset";
+$locale['0520'] = "Offset position of captions from top border of the slide";
+$locale['0521'] = "Left";
+$locale['0522'] = "Right";
+$locale['0523'] = "Center";
+$locale['0524'] = "Caption Text Alignment";
+$locale['0525'] = "Caption Title Size";
+$locale['0526'] = "Size of title in px. Negative number not allowed.";
+$locale['0527'] = "Caption Description Size";
+$locale['0528'] = "Size of description in px. Negative number not allowed.";
+$locale['0529'] = "Normal Size";
+$locale['0530'] = "Small Size";
+$locale['0531'] = "Medium Size";
+$locale['0532'] = "Large Size";
+$locale['0533'] = "Link Button Size";
+$locale['0534'] = "Base Image Path";
+$locale['0535'] = "Base Image Directory";
+$locale['0600'] = "Save Slider";
+$locale['0601'] = "Save and Close Slider";
