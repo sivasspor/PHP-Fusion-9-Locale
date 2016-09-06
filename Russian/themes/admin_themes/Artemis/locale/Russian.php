b5ab851c098f98f75d2dab5c0d081fca339b72bf
Rizado <v.chubatyj@yandex.ru>
2016-09-06 22:15:46 +0300

    Added Artemis admin theme locale

---
+++ Russian/themes/admin_themes/Artemis/locale/Russian.php
@@ -0,0 +1,6 @@
+<?php
+$locale['spotlight'] = "Центр внимания";
+$locale['spotlight_search'] = "Поиск приложений";
+$locale['copyright'] = "Авторские права &copy; ";
+$locale['render_engine'] = "Движок отрисовки";
+$locale['artemis_admin'] = "Интерфейс администратора Artemis для PHP-Fusion 9";
