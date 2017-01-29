c1ef5a1dcc88d3d30a101b285c70c4baca2f8cde
Rizado <v.chubatyj@yandex.ru>
2017-01-29 11:11:05 +0300

    User Fields updated

---
+++ Russian/locale/Russian/user_fields/user_facebook.php
@@ -0,0 +1,6 @@
+<?php
+$locale['uf_facebook'] = "Facebook";
+$locale['uf_facebook_desc'] = "Профиль Facebook";
+$locale['uf_facebook_error'] = "Поле Facebook не может быт пустым.";
+$locale['uf_facebook_palceholder'] = "Ссылка на ваш профиль (например, phfusion )";
+$locale['uf_facebook_tip'] = "https://www.facebook.com/ это должно быть казано после, например, phfusion";
