33816a74e96238c4cb1a7da91e74f5c3ed08b1d1
ALTUN <altunmuhammet69@hotmail.com>
2017-03-19 21:25:30 +0100

    Adapted to PHP-Fusion 9.02

--- Turkish/locale/Turkish/contact.php
+++ Turkish/locale/Turkish/contact.php
@@ -1,27 +1,24 @@
 <?php
 // Contact Form
 $locale['400'] = "İletişim Formu";
-$locale['401'] = "Bize ulaşmanız için pek çok yol mevcut, 
-".hide_email(fusion_get_settings('siteemail'))." adresine doğrudan email gönderebilirsiniz. Eğer sitemize üyeyseniz
-<a href='messages.php?msg_send=1'>Özel Mesaj</a> ile bizle iletişim kurabilirsiniz. Alternatif olarak, bu sayfadaki formu doldurarak
-E-posta ile bize mesaj gönderebilirsiniz.";
+$locale['401'] = "Bizimle iletişime geçmenin birkaç yolu vardır. Bana doğrudan  
+[SITE_EMAIL] adresinden E-posta gönderebilirsiniz. Eğer bir üyeyseniz bana [PM_LINK] gönderebilirsiniz.
+Alternatif olarak, bu sayfadaki formu doldurarak E-posta ile bize mesaj gönderebilirsiniz.";
 $locale['402'] = "İsim:";
 $locale['403'] = "E-posta Adresi:";
 $locale['404'] = "Konu:";
 $locale['405'] = "Mesaj:";
-$locale['406'] = "Mesajı Gönder";
+$locale['406'] = "Mesaj Gönder";
 $locale['407'] = "Doğrulama Kodu:";
 $locale['408'] = "Doğrulama Kodunu Girin:";
 // Contact Errors
 $locale['420'] = "Bir isim belirtmelisiniz";
-$locale['421'] = "Bir E-posta Adresi belirtmelisiniz";
-$locale['422'] = "Bir Konu belirtmelisiniz";
-$locale['423'] = "Bir Mesaj belirtmelisiniz";
-$locale['424'] = "Hatalı doğrulama kodu, lütfen doğrulama kodunu yeniden girin";
-$locale['425'] = "Dahili hata: mesajınız gönderilemedi.";
+$locale['421'] = "Bir E-posta adresi belirtmelisiniz";
+$locale['422'] = "Bir konu belirtmelisiniz";
+$locale['423'] = "Bir mesaj belirtmelisiniz";
+$locale['424'] = "Doğru Doğrulama Kodu girmelisiniz";
+$locale['425'] = "Dahili hata: mesajınız gönderilemedi..";
 // Message Sent
-$locale['440'] = "Mesajınız başarı ile gönderildi";
+$locale['440'] = "Mesajınız gönderildi";
 $locale['441'] = "Teşekkürler";
-$locale['442'] = "Mesajınız aşağıdaki sebep(ler)den dolayı gönderilemedi:";
 $locale['443'] = "Lütfen tekrar deneyin.";
-
