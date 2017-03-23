3137f290f5475bd5e6a5410ba9fe7bb7e2c2f0b0
kot2007 <koot2007@gmail.com>
2017-03-23 16:47:04 +0200

    Ukrainian locale sync

--- Ukrainian/infusions/articles/locale/Ukrainian/articles_admin.php
+++ Ukrainian/infusions/articles/locale/Ukrainian/articles_admin.php
@@ -1,101 +1,164 @@
 <?php
 // Article titles
-$locale['articles_0000'] = "Наявні статті";
-$locale['articles_0001'] = "Статті";
-$locale['articles_0002'] = "Створення статті";
-$locale['articles_0003'] = "Редагування статті";
-// Category titles
-$locale['articles_0020'] = "Наявні розділи статей";
-$locale['articles_0021'] = "Створення розділу статей";
-$locale['articles_0022'] = "Редагування розділу статей";
-$locale['articles_0023'] = "Всі статті";
-$locale['articles_0024'] = "Відображається %d з %d статей";
-$locale['articles_0025'] = "Сортування за:";
-$locale['articles_0026'] = "Статті відсортовано за";
-$locale['articles_0027'] = "Редагування розділу статей";
-// Article Settings
-$locale['articles_0030'] = "Налаштування";
-$locale['articles_0031'] = "Налаштування статей";
-$locale['articles_0032'] = "Кількість статей на одній сторінці";
-$locale['articles_0033'] = "Можливість надсилання статей для ухвалення?";
-$locale['articles_0034'] = "Передбачається повний зміст?";
-// Article Submissions
-$locale['articles_0040'] = "Ухвалення статей";
-$locale['articles_0041'] = "Заголовок";
-$locale['articles_0042'] = "Статті для ухвалення відсутні";
-$locale['articles_0043'] = "Вибачте, на жаль, сайт зараз не приймає пропозиції статей.";
-$locale['articles_0044'] = "Кількість статей, що очікують ухвалення: %s.";
-$locale['articles_0045'] = "Попередній перегляд";
-$locale['articles_0046'] = "Автор";
-$locale['articles_0047'] = "Час";
-$locale['articles_0048'] = "Номер ID";
-$locale['articles_0049'] = "Статтю для ухвалення успішно видалено";
-$locale['articles_0050'] = "Статтю для ухвалення успішно опубліковано";
-$locale['articles_0051'] = "Статтю для ухвалення збережено як чернетку";
-$locale['articles_0052'] = "Пропозиція статті надійшла від: ";
-$locale['articles_0053'] = "Опубліковано: ";
-$locale['articles_0060'] = "Надіслати";
-$locale['articles_0061'] = "Дякуємо за Вашу пропозицію статті";
-$locale['articles_0062'] = "Запропонувати ще";
-$locale['articles_0063'] = "Використайте нижченаведену форму для надсилання статті. Вашу пропозицію буде розглянуто адміністратором. Сайт [SITENAME] залишає за собою право виправляти або змінювати будь-яку пропозицію. Майте на увазі, що стаття має відповідати тематиці та правилам сайту. Недопустимі пропозиції будуть видалені.";
-$locale['articles_0064'] = "Повернутися до початку сайту [SITENAME]";
-// Article messages
-$locale['articles_0100'] = "Статтю успішно створено";
-$locale['articles_0101'] = "Статтю успішно оновлено";
-$locale['articles_0102'] = "Статтю успішно видалено";
-// Category messages
-$locale['articles_0150'] = "Розділ статей успішно створено";
-$locale['articles_0151'] = "Розділ статей успішно оновлено";
-$locale['articles_0152'] = "Розділ статей неможливо видалити";
-$locale['articles_0153'] = "У цьому розділі є статті або інші підрозділи, спочатку видаліть їх.";
-$locale['articles_0154'] = "Розділ статей успішно видалено";
-// Article form
-$locale['articles_0200'] = "Тема:";
-$locale['articles_0201'] = "Розділ:";
-$locale['articles_0202'] = "Анонс:";
-$locale['articles_0203'] = "Повний зміст:";
-$locale['articles_0204'] = "Ключові слова";
-$locale['articles_0204a'] = "Натискайте Enter після кожного слова";
-$locale['articles_0205'] = "Зберегти як чернетку";
-$locale['articles_0206'] = "Автоматичне перенесення рядків";
-$locale['articles_0207'] = "Дозволити коментарі";
-$locale['articles_0208'] = "Дозволити оцінювання";
-$locale['articles_0209'] = "Розрив стор.";
-$locale['articles_0210'] = "[чернетка]";
-$locale['articles_0211'] = "Доступ";
-$locale['articles_0240'] = "Попередній перегляд";
-$locale['articles_0241'] = "Зберегти";
-$locale['articles_0242'] = "Опублікувати";
-$locale['articles_0243'] = "Видалити";
-// Popup Error/Warning Messages
-$locale['articles_0250'] = "Будь ласка, вкажіть заголовок статті";
-$locale['articles_0251'] = "Видалити цю статтю?";
-$locale['articles_0252'] = "Відсутні розділи статей.";
-$locale['articles_0253'] = "Щоб створювати статті, потрібно спочатку створити хоча б один розділ.";
-$locale['articles_0254'] = "Натисніть тут";
-$locale['articles_0255'] = " , щоб перейти до налаштування розділів статей.";
-$locale['articles_0256'] = "%s заборонено для усіх.\n\rЦі налаштування матимуть силу лише після їх дозволу.";
-$locale['articles_0257'] = "Будь ласка, вкажіть ключові слова";
-// Category form
-$locale['articles_0300'] = "Назва:";
-$locale['articles_0301'] = "Опис:";
-$locale['articles_0302'] = "Сортування записів у розділі за:";
-$locale['articles_0303'] = "номером";
-$locale['articles_0304'] = "назвою";
-$locale['articles_0305'] = "датою";
-$locale['articles_0306'] = "за зростанням";
-$locale['articles_0307'] = "за спаданням";
-$locale['articles_0308'] = "Первинний розділ:";
-$locale['articles_0309'] = "Зберегти";
-// Current category options
-$locale['articles_0340'] = "Назва";
-$locale['articles_0341'] = "Опції";
-$locale['edit'] = "Редагувати";
-$locale['delete'] = "Видалити";
-$locale['articles_0342'] = "Розділи відсутні";
-$locale['articles_0343'] = "Статті відсутні";
-// Delete Article category
-$locale['articles_0350'] = "Видалити цей розділ статей?";
-// Error Messages
-$locale['articles_0351'] = "Будь ласка, вкажіть назву розділу.";
-$locale['articles_0352'] = "Розділ з такою назвою вже існує.";
+$locale['article_0000'] = "Наявні статті";
+$locale['article_0001'] = "Статті";
+$locale['article_0002'] = "Створення статті";
+$locale['article_0003'] = "Редагування статті";
+$locale['article_0004'] = "Розділи";
+$locale['article_0005'] = "Додати розділ";
+$locale['article_0006'] = "Редагувати розділ";
+$locale['article_0007'] = "Ухвалення";
+$locale['article_0008'] = "Налаштування";
+
+// Messages - Articles
+$locale['article_0030'] = "Статтю успішно створено";
+$locale['article_0031'] = "Статтю успішно оновлено";
+$locale['article_0032'] = "Статтю успішно видалено";
+$locale['article_0033'] = "Статті успішно оновлено";
+$locale['article_0034'] = "Виберіть, будь ласка, хоча б одну статтю";
+
+// Messages - Articles Cats
+$locale['article_0040'] = "Розділ успішно створено";
+$locale['article_0041'] = "Розділ успішно оновлено";
+$locale['article_0042'] = "Розділ успішно видалено";
+$locale['article_0043'] = "Розділ неможливо видалити";
+$locale['article_0044'] = "У цьому розділі є статті або інші підрозділи, спочатку видаліть їх.";
+$locale['article_0045'] = "Розділи успішно оновлено";
+$locale['article_0046'] = "Розділи неможливо видалити";
+$locale['article_0047'] = "У цих розділах є статті або інші підрозділи, спочатку видаліть їх.";
+$locale['article_0048'] = "Виберіть, будь ласка, хоча б один розділ";
+
+// Messages - Submissions
+$locale['article_0060'] = "Статтю для ухвалення успішно опубліковано";
+$locale['article_0061'] = "Статтю для ухвалення успішно збережено як чернетку";
+$locale['article_0062'] = "Статтю для ухвалення успішно видалено";
+$locale['article_0063'] = "Статті для ухвалення відсутні";
+
+// Table - Articles
+$locale['article_0100'] = "Заголовок";
+$locale['article_0101'] = "Розділ";
+$locale['article_0102'] = "Чернетка";
+$locale['article_0103'] = "Коментарі";
+$locale['article_0104'] = "Оцінки";
+$locale['article_0105'] = "Автор";
+$locale['article_0106'] = "Доступ";
+$locale['article_0107'] = "Дії";
+
+// Table - Articles - Misc
+$locale['article_0110'] = "Створити статтю";
+$locale['article_0111'] = "Ви дійсно бажаєте видалити цю статтю?";
+$locale['article_0112'] = "Жодної статті не створено.";
+$locale['article_0113'] = "Не знайдено жодної статті за вказаними параметрами пошуку";
+$locale['article_0114'] = "Перш ніж створювати статтю, необхідно створити хоча б один розділ для статей";
+
+// Table - Articles - Sorting
+$locale['article_0120'] = "Заголовок";
+$locale['article_0121'] = "Пошук";
+$locale['article_0122'] = "Очистити";
+$locale['article_0123'] = "Стан";
+$locale['article_0124'] = "Всі статуси";
+$locale['article_0125'] = "Доступ";
+$locale['article_0126'] = "Розділ";
+$locale['article_0127'] = "Всі розділи";
+$locale['article_0128'] = "Мова";
+$locale['article_0129'] = "Всі мови";
+$locale['article_0130'] = "Автор";
+$locale['article_0131'] = "Всі автори";
+$locale['article_0132'] = "Кількість на одній сторінці";
+
+// Table - Articles Cats
+$locale['article_0150'] = "Назва";
+$locale['article_0151'] = "Статті";
+$locale['article_0152'] = "Стан";
+$locale['article_0153'] = "Доступ";
+
+// Table - Articles Cats - Misc
+$locale['article_0160'] = "Створити розділ";
+$locale['article_0161'] = "Ви дійсно бажаєте видалити цей розділ?";
+$locale['article_0162'] = "Розділи для статей відсутні";
+
+// Table - Article Cats - Sort
+$locale['article_0170'] = "Назва";
+$locale['article_0171'] = $locale['article_0121'];
+$locale['article_0172'] = $locale['article_0122'];
+$locale['article_0173'] = $locale['article_0123'];
+$locale['article_0174'] = $locale['article_0124'];
+$locale['article_0175'] = $locale['article_0125'];
+$locale['article_0176'] = $locale['article_0128'];
+$locale['article_0177'] = $locale['article_0129'];
+
+// Table - Submissions
+$locale['article_0200'] = "Код ID";
+$locale['article_0201'] = "Заголовок";
+$locale['article_0202'] = "Автор";
+$locale['article_0203'] = "Дата";
+$locale['article_0204'] = "Опції";
+$locale['article_0205'] = "Перегляд";
+
+// Form - Articles
+$locale['article_0250'] = "Заголовок:";
+$locale['article_0251'] = "Анонс:";
+$locale['article_0251a'] = "Витяг зі статті для короткого ознайомлення читача зі змістом.";
+$locale['article_0252'] = "Зміст:";
+$locale['article_0252a'] = "Повний зміст статті.";
+$locale['article_0253'] = "Розділ:";
+$locale['article_0254'] = "Доступ:";
+$locale['article_0255'] = "Дата";
+$locale['article_0256'] = "Зберегти як чернетку";
+$locale['article_0257'] = "Автоматичне перенесення рядків";
+$locale['article_0258'] = "Коментарі";
+$locale['article_0259'] = "Оцінки";
+$locale['article_0260'] = "Ключові слова";
+$locale['article_0260a'] = "Розділюйте кожне слово через Enter.";
+$locale['article_0261'] = "Додатково";
+$locale['article_0262'] = "Налаштування";
+
+// Form - Articles - Errors
+$locale['article_0270'] = "Будь ласка, вкажіть заголовок статті.";
+$locale['article_0271'] = "Будь ласка, вкажіть анонс статті.";
+$locale['article_0272'] = "Будь ласка, вкажіть зміст статті.";
+$locale['article_0273'] = "Будь ласка, виберіть розділ для статті.";
+$locale['article_0274'] = "%s заборонено для всього.\n\rЦі налаштування стануть чинними, коли будуть знову ввімкнені.";
+
+// Form - Articles Cats
+$locale['article_0302'] = "Назва";
+$locale['article_0303'] = "Первинний";
+$locale['article_0304'] = "Опис";
+$locale['article_0306'] = "Доступ";
+$locale['article_0307'] = "Статус";
+$locale['article_0308'] = "Додатково";
+
+// Form - Articles Cats - Error
+$locale['article_0320'] = "Будь ласка, вкажіть назву розділу.";
+$locale['article_0321'] = "Розділ з такою назвою вже існує.";
+$locale['article_0322'] = "Будь ласка, вкажіть опис розділу.";
+
+// Form - Submissions
+$locale['article_0350'] = "Статтю для ухвалення надіслано від {%SUBMISSION_AUTHOR%}.";
+$locale['article_0351'] = "Дата запиту: {%SUBMISSION_TIME%} - {%SUBMISSION_DATE%}.";
+$locale['article_0352'] = "Опублікувати";
+
+// Settings
+$locale['article_0400'] = "Налаштування статей для користувацького (frontend) та серверного (Backend) представлення.";
+$locale['article_0401'] = "Кількість статей на одній сторінці";
+$locale['article_0402'] = "Ухвалення";
+$locale['article_0403'] = "Необхідна розширена стаття";
+
+/*
+--- PUBLIC Submission ---
+*/
+
+// Submissions - Titles
+$locale['article_0900'] = "Ухвалення статті";
+
+// Submissions - Success Submission
+$locale['article_0910'] = "Пропозицію статті успішно надіслано для ухвалення.";
+$locale['article_0911'] = "Дякуємо за Вашу пропозицію статті.";
+$locale['article_0912'] = "Запропонувати ще";
+$locale['article_0913'] = "Повернутися на сайт";
+
+// Submissions - Other
+$locale['article_0920'] = "Використайте нижченаведену форму для надсилання статті. Вашу пропозицію буде розглянуто адміністратором. Сайт [SITENAME] залишає за собою право виправляти або змінювати будь-яку пропозицію. Майте на увазі, що стаття має відповідати тематиці та правилам сайту. Недопустимі пропозиції будуть видалені.";
+$locale['article_0921'] = "Ви повинні авторизуватися для надсилання пропозиції статті.";
+$locale['article_0922'] = "Вибачте, на жаль, сайт зараз не приймає пропозиції статей.";
+$locale['article_0923'] = "Ви не можете запропонувати статтю, оскільки відсутні розділи для статей.";
