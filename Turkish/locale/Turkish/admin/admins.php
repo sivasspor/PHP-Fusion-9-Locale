2553b703cc510b2c6e9efe53a1d9f999f23b4a21
sivasspor <yusufulku58@hotmail.com>
2017-03-24 22:11:11 +0200

    Update admins.php

--- Turkish/locale/Turkish/admin/admins.php
+++ Turkish/locale/Turkish/admin/admins.php
@@ -1,46 +1,46 @@
 <?php
 // Administrators messages
-$locale['400'] = "Administrator added";
-$locale['401'] = "Administrator updated";
-$locale['402'] = "Administrator removed";
+$locale['400'] = "Yönetici eklendi";
+$locale['401'] = "Yönetici güncellendi";
+$locale['402'] = "Yönetici kaldırıldı";
 // Search/Add Administrator
-$locale['410'] = "Add Administrator";
+$locale['410'] = "Yönetici Ekle";
 $locale['411'] = "Search for users to add using the form below.";
-$locale['412'] = "User ID";
-$locale['413'] = "User Name";
-$locale['414'] = "Search";
+$locale['412'] = "Kullanıcı ID";
+$locale['413'] = "Kullanıcı Adı";
+$locale['414'] = "Ara";
 $locale['415'] = "Apply all privileges";
 $locale['416'] = "Make Super Administrator";
 $locale['417'] = "Add Admin";
 $locale['418'] = "No users found.";
-$locale['419'] = "Search Again";
+$locale['419'] = "Tekrar Ara";
 // Current Administrators
 $locale['420'] = "Administrators";
 $locale['421'] = "User Name";
 $locale['422'] = "Admin Level";
-$locale['423'] = "Options";
+$locale['423'] = "Seçenekler";
 $locale['424'] = "Primary Admin";
 $locale['425'] = "No rights assigned";
-$locale['426'] = "Edit";
-$locale['427'] = "Remove";
+$locale['426'] = "Düzenle";
+$locale['427'] = "Kaldır";
 // Edit Admin Rights
 $locale['440'] = "Edit Admin Rights";
 $locale['441'] = "Content Admin";
 $locale['442'] = "User Admin";
 $locale['443'] = "System Admin";
-$locale['444'] = "Infusions";
+$locale['444'] = "Eklentiler";
 $locale['445'] = "Check all";
 $locale['446'] = "UnCheck all";
 $locale['447'] = "Admin Password:";
 $locale['448'] = "Save Admin Rights";
-$locale['449'] = "Settings";
+$locale['449'] = "Ayarlar";
 $locale['450'] = "Check secure";
 // Popup Error/Warning Messages
-$locale['460'] = "Remove this administrator?";
+$locale['460'] = "Bu yöneticiyi kaldırmak istiyor musun?";
 $locale['461'] = "Add this administrator?";
 $locale['462'] = "Giving these admin rights to a user is a potential security risk!";
 // Remove Admin confirmation
-$locale['470'] = "Delete Admin";
+$locale['470'] = "Admin Sil";
 $locale['471'] = "In order to remove this Administrator, please enter your admin password:";
-$locale['472'] = "Confirm";
-$locale['473'] = "Cancel";
+$locale['472'] = "Onayla";
+$locale['473'] = "İptal";
