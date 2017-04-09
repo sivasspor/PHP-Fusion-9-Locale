fc41fc187430889b77273b657ca28b7790accd18
Helmuth Mikkelsen <helmuthm@gmail.com>
2017-04-09 06:32:45 +0200

    changed 'indlæg' to 'nyhed'

--- Danish/themes/FusionTheme/locale/Danish.php
+++ Danish/themes/FusionTheme/locale/Danish.php
@@ -26,4 +26,4 @@ $locale['NB_103'] = "Data om brugerforslag";
 // Nebula News Template
 $locale['NB_200'] = "af";
 // News Module
-$locale['NEWS_001'] = "Seneste indlæg";
+$locale['NEWS_001'] = "Seneste nyhed";
