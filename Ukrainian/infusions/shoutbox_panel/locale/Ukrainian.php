b2a58222b023f18d9312b95ee588cebfed780f7c
kot2007 <koot2007@gmail.com>
2017-04-11 16:16:45 +0300

    Ukraininan locale sync

--- Ukrainian/infusions/shoutbox_panel/locale/Ukrainian.php
+++ Ukrainian/infusions/shoutbox_panel/locale/Ukrainian.php
@@ -1,58 +1,37 @@
 <?php
-$locale['SB_title'] = "Чат";
-$locale['SB_desc'] = "Чат для вільного спілкування користувачів";
-$locale['SB_admin1'] = "Керування чатом";
-
-// Shoutbox
-$locale['SB_name'] = "Ім&lsquo;я:";
-$locale['SB_message'] = "Повідомлення:";
-$locale['SB_shout'] = "Надіслати";
-$locale['SB_login_req'] = "Для можливості дописування в чат Ви маєте авторизуватися на сайті.";
-$locale['SB_archive'] = "Архів";
-$locale['SB_no_msgs'] = "Повідомлення відсутні";
-$locale['SB_delete'] = "Видалити";
-$locale['SB_validation_code'] = "Код для перевірки:";
-$locale['SB_enter_validation_code'] = "Введіть код для перевірки:";
-$locale['SB_edit_shout'] = "Редагувати";
-
-// Shoutbox messages
-$locale['SB_shout_added'] = "Повідомлення успішно створено";
-$locale['SB_shout_updated'] = "Повідомлення успішно оновлено";
-$locale['SB_shout_deleted'] = "Повідомлення успішно видалено";
-$locale['SB_shouts_deleted'] = "Повідомлення успішно видалено";
-$locale['SB_shout_captha'] = "Введіть код безпеки:";
-
-// Edit Shout form
-$locale['SB_save_shout'] = "Зберегти";
-$locale['SB_update_shout'] = "Оновити";
-$locale['SB_selected_shout'] = "Видалити";
-
-// Prune Shoutbox
-$locale['SB_delete_old'] = "Видалити повідомлення старші ніж";
-$locale['SB_days'] = " дн.";
-$locale['SB_delete_shouts'] = "Видалити";
-
-// Current Shouts
-$locale['SB_edit'] = "Редагувати";
 $locale['SB_add'] = "Нове повідомлення";
-$locale['SB_userip'] = "Адреса IP: ";
-$locale['SB_on_date'] = " ";
-$locale['SB_lang'] = "Мова: ";
-$locale['SB_visbility'] = "Доступ: ";
-
-// Popup Error/Warning Messages
-$locale['SB_warning_shouts'] = "Ви дійсно бажаєте видалити повідомлення?";
-$locale['SB_warning_shout'] = "Ви дійсно бажаєте видалити повідомлення?";
+$locale['SB_admin1'] = "Керування чатом";
+$locale['SB_archive'] = "Архів";
+$locale['SB_days'] = "дн.";
+$locale['SB_delete_old'] = "Видалити повідомлення старші ніж";
+$locale['SB_delete_shouts'] = "Видалити";
+$locale['SB_desc'] = "Чат для вільного спілкування користувачів";
+$locale['SB_edit_shout'] = "Редагувати";
+$locale['SB_enter_validation_code'] = "Введіть код для перевірки:";
 $locale['SB_entries'] = "Відображено %d з %d повідомлень.";
-$locale['SB_noentries'] = "Не вибрано жодного повідомлення.";
 $locale['SB_flood'] = "Флуд! Ви надто часто пишете повідомлення. Будь ласка, зачекайте %s сек. перед написанням наступного повідомлення.";
-
-// Settings
-$locale['SB_settings'] = "Налаштування";
-$locale['SB_visible_shouts'] = "Доступ до перегляду";
 $locale['SB_guest_shouts'] = "Повідомлення від гостей";
 $locale['SB_hidden_shouts'] = "Налаштування доступу";
-$locale['SB_yes'] = "так";
-$locale['SB_no'] = "ні";
-$locale['SB_submit'] = "Зберегти";
+$locale['SB_lang'] = "Мова: ";
+$locale['SB_login_req'] = "Для можливості дописування в чат Ви маєте авторизуватися на сайті.";
+$locale['SB_message'] = "Повідомлення:";
+$locale['SB_name'] = "Ім&lsquo;я:";
+$locale['SB_no_msgs'] = "Повідомлення відсутні";
+$locale['SB_noentries'] = "Не вибрано жодного повідомлення.";
+$locale['SB_save_shout'] = "Зберегти";
+$locale['SB_selected_shout'] = "Видалити";
+$locale['SB_settings'] = "Налаштування";
+$locale['SB_shout'] = "Надіслати";
+$locale['SB_shout_added'] = "Повідомлення успішно створено";
+$locale['SB_shout_captha'] = "Введіть код безпеки:";
+$locale['SB_shout_deleted'] = "Повідомлення успішно видалено";
+$locale['SB_shout_updated'] = "Повідомлення успішно оновлено";
+$locale['SB_shouts_deleted'] = "Повідомлення успішно видалено";
+$locale['SB_title'] = "Чат";
 $locale['SB_update_ok'] = "Налаштування успішно оновлено";
+$locale['SB_update_shout'] = "Оновити";
+$locale['SB_userip'] = "Адреса IP:";
+$locale['SB_visbility'] = "Доступ:";
+$locale['SB_visible_shouts'] = "Доступ до перегляду";
+$locale['SB_warning_shout'] = "Ви дійсно бажаєте видалити повідомлення?";
+$locale['SB_warning_shouts'] = "Ви дійсно бажаєте видалити повідомлення?";
--- Ukrainian/locale/Ukrainian/user_fields/user_name_last.php
+++ Ukrainian/locale/Ukrainian/user_fields/user_name_last.php
@@ -2,4 +2,3 @@
 $locale['uf_name_last'] = "Прізвище";
 $locale['uf_name_last_desc'] = "Прізвище користувача";
 $locale['uf_name_last_error'] = "Поле прізвища не може бути порожнім";
-
