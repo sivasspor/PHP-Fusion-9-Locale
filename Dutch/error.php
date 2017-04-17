0e44445142838686cbae1dc532fb42e650649970
douweyntema <douweyntema@users.noreply.github.com>
2017-04-17 13:21:02 +0200

    Add files via upload

---
+++ Dutch/error.php
@@ -0,0 +1,8 @@
+<?php
+$locale['err401'] = "Ongeoorloofde toegang tot deze pagina!";
+$locale['err403'] = "Geen toegang tot deze pagina!";
+$locale['err404'] = "Deze pagian is niet gevonden op deze server!";
+$locale['err500'] = "Interne server fout!";
+$locale['errunk'] = "Onbekende fout";
+$locale['errret'] = "Terug naar de main site";
+
---
+++ Dutch/members.php
@@ -0,0 +1,13 @@
+<?php
+$locale['400'] = "Ledenlijst";
+$locale['401'] = "Gebruikersnaam";
+$locale['402'] = "Gebruikerstype";
+$locale['403'] = "Er zijn geen gebruikersnamen beginnend met ";
+$locale['404'] = "Toon alle";
+$locale['405'] = "Gebruikersgroepen";
+$locale['406'] = "Niet opgenomen in een gebruikersgroep";
+$locale['407'] = "Toegang tot alle groepen";
+$locale['408'] = "Zoeken naar een lid:";
+$locale['409'] = "Zoeken";
+
+
