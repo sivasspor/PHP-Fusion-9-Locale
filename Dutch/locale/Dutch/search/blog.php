b04da3e9e2c2c32ef99706d554a7bdc35691446e
douweyntema <douweyntema@users.noreply.github.com>
2017-04-21 19:53:16 +0200

    Translated Search

---
+++ Dutch/locale/Dutch/search/blog.php
@@ -0,0 +1,8 @@
+<?php
+$locale['n400'] = "Blog";
+$locale['n401'] = "Blog bericht";
+$locale['n402'] = "Blog berichten";
+$locale['n403'] = "gevonden in";
+$locale['n404'] = "Blog onderwerp";
+$locale['n405'] = "Blog tekst";
+$locale['n406'] = "uitgebreide Blog tekst";
