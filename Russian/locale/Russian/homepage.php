4ef23e0fa72b022b6dfb6da82909b4a069a6a611
Rizado <v.chubatyj@yandex.ru>
2017-04-22 14:59:21 +0300

    Russian locale update
    Main dir

--- Russian/locale/Russian/homepage.php
+++ Russian/locale/Russian/homepage.php
@@ -4,12 +4,6 @@ $locale['home_0000'] = "Последние новости";
 $locale['home_0001'] = "Последние статьи";
 $locale['home_0002'] = "Последние записи блогов";
 $locale['home_0003'] = "Последние файлы";
-$locale['submit_0000'] = "Новостей: %s";
-$locale['submit_0001'] = "Статей: %s";
-$locale['submit_0002'] = "Файлов: %s";
-$locale['submit_0003'] = "Фото: %s";
-$locale['submit_0004'] = "Ссылок: %s";
-$locale['submit_0005'] = "Записей блогов: %s";
 // no content messages
 $locale['home_0050'] = "Нет новостей для отображения";
 $locale['home_0051'] = "Нет статей для отображения";
