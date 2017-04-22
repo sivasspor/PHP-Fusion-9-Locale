6fef38f7229949642d2e0c48dd177ef9e671dfbe
RobiNN <robokelcak@centrum.sk>
2017-04-22 16:18:57 +0200

    Delete eshop-cats.php

--- Dutch/locale/Dutch/permalinks/eshop-cats.php
+++
@@ -1,6 +0,0 @@
-<?php
-$locale['pl_eshop_cats_title'] = "eShop Categorie";
-$locale['pl_eshop_cats_desc'] = "Inschakelen Permalinks  voor de eShop Categorie op de site.";
-$locale['pl_tags_001'] = "ID van het eShop Categorie Item.";
-$locale['pl_tags_002'] = "Titel of Onderwerp van het eShop Categorie Item.";
-
