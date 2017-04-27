8b4ca73f33e1ba89403f40c46deb5b53a7293a62
Rizado <v.chubatyj@yandex.ru>
2017-04-27 14:03:44 +0300

    Russian locale update
    BB codes are completed

--- Russian/locale/Russian/bbcodes/url.php
+++ Russian/locale/Russian/bbcodes/url.php
@@ -1,5 +1,5 @@
 <?php
-$locale['bb_url_displayas'] = "Оторажение активной ссылки";
+$locale['bb_url_displayas'] = "Отображение активной ссылки";
 $locale['bb_url_description'] = "Преобразование ссылки в активную";
 $locale['bb_url_usage'] = "адрес";
 
