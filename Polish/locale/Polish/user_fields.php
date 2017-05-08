22eeaf6c66d99f59d0df62795b3cb2b351e447ac
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 20:35:25 +0200

    Update user_fields.php

--- Polish/locale/Polish/user_fields.php
+++ Polish/locale/Polish/user_fields.php
@@ -50,8 +50,7 @@ $locale['uf_104'] = "User Info";
 $locale['uf_105'] = "Profile Picture";
 $locale['uf_106'] = "Basic Information";
 $locale['uf_107'] = "%s has no user information available.";
-$locale['uf_108'] = "User fields not found.";
-
+$locale['uf_108'] = "There are no user fields defined";
 
 // View User Groups
 $locale['u110'] = "View User Group";
@@ -59,6 +58,8 @@ $locale['u111'] = "%u user";
 $locale['u112'] = "%u users";
 $locale['u113'] = "User Name";
 $locale['u114'] = "User Type";
+$locale['u115'] = "Language";
+$locale['u116'] = "There are no group members.";
 // User name and email
 $locale['u120'] = "User name contains invalid characters.";
 $locale['u121'] = "The chosen user name is already taken by another user.";
@@ -100,7 +101,7 @@ $locale['u148a'] = "New Admin Passwords are not identical.";
 $locale['u149'] = "Your Current Login Password was not specified or is invalid.";
 $locale['u149a'] = "Your Current Admin Password was not specified or is invalid.";
 $locale['u149b'] = "Your Current Login Password was not specified or is invalid.\n\rYou can't set your admin password without your correct login password.";
-// Email actiation
+// Email activation
 $locale['u150'] = "Your registration is almost complete, you will receive an email containing your login details along with a link to verify your account.";
 $locale['u151'] = "Welcome to [SITENAME]";
 $locale['u152'] = "Hello USER_NAME,\n\r
@@ -108,19 +109,21 @@ Welcome to SITENAME. Here are your login details:\n\r
 Username: USER_NAME\n\r
 Password: USER_PASSWORD\n\r
 Please activate your account via the following link: ACTIVATION_LINK\n\n\r\r
-Regards,
+Regards,\n\r
 SITEUSERNAME";
+
 $locale['u153'] = "Activation email could not be sent.";
 $locale['u154'] = "Please [LINK]contact[/LINK] the Site Administrator.";
 $locale['u155'] = "Activate Account";
-$locale['u156'] = "Please type in your current password to change your email.";
+$locale['u156'] = "Please type in current password to change email.";
 // Success / Fail
 $locale['u160'] = "Registration complete";
 $locale['u161'] = "You can now log in.";
 $locale['u162'] = "An administrator will activate your account shortly.";
 $locale['u163'] = "Profile was successfully updated.";
 $locale['u164'] = "Update failed";
-$locale['u165'] = "Registration failed";
+
+//$locale['u165'] = "Registration failed";
 $locale['u167'] = "for the following reason(s):";
 $locale['u168'] = "Please Try Again.";
 $locale['u169'] = "Profile updated";
@@ -130,10 +133,8 @@ $locale['u172'] = "Member successfully added.";
 $locale['u173'] = "Back to User Management.";
 $locale['u174'] = "Add another member.";
 // Avatar upload
-$locale['u180'] = "Your avatar exceeded file size allowed, the limit is %s.";
-$locale['u181'] = "Your avatar appears to be an unsupported image type, supported image types are jpg, png and gif.";
-$locale['u182'] = "Your avatar exceeded %s x %s pixels.";
-$locale['u183'] = "Your avatar was not uploaded correctly.";
+$locale['u180'] = "There is an error uploading avatar image";
+
 $locale['u184'] = "Max. file size: %s / Max. size: %ux%u pixels";
 $locale['u185'] = "Avatar";
 $locale['u186'] = "Click Browse to upload an image";
@@ -143,17 +144,21 @@ $locale['u190'] = "Validation Code";
 $locale['u191'] = "Enter Validation Code";
 $locale['u192'] = "Terms of Agreement";
 $locale['u193'] = "I have read the [LINK]Terms of Agreement[/LINK] and I agree with them.";
+$locale['u193a'] = "Updated";
+$locale['u193b'] = "I agree";
 $locale['u194'] = "Incorrect validation code.";
 $locale['u195'] = "Captcha code can not be left empty.";
 // E-mail Change Confirmation
-$locale['u200'] = "A verify email has been sent to your new email address (%s).";
+$locale['u200'] = "A verify email has been sent to your new email address (%s). Please check your email.";
 $locale['u201'] = "Your email address will be changed when you click the link in the mail.";
 $locale['u202'] = "E-mail address verify - [SITENAME]";
-$locale['u203'] = "Hello [USER_NAME],\n\r
-Someone set this email address in his account on our site.\n\r
-If you realy want to change your email address to this one please click the following link:\n\r
-[EMAIL_VERIFY_LINK]\n\r
-Note: you have to be logged in to proceed.\n\r
-Regards,\n\r
+$locale['u203'] = "Hello [USER_NAME],\n
+Someone set this email address in his account on our site.\n
+If you realy want to change your email address to this one please click the following link:\n
+[EMAIL_VERIFY_LINK]\n
+Note: you have to be logged in to proceed.\n
+Regards,
 [SITEUSERNAME]\n\r
 [SITENAME]";
+
+$locale['u210'] = "User Reputation";
