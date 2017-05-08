a1ec220fa511e7a2316b536b3634534c80c3ebe5
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 19:27:38 +0200

    Update left.php

--- Polish/locale/Polish/bbcodes/left.php
+++ Polish/locale/Polish/bbcodes/left.php
@@ -1,4 +1,4 @@
 <?php
-$locale['bb_left_description'] = "Wyrównuje tekst do lewego marginesu";
+$locale['bb_left_description'] = "Wyrównuje zaznaczony tekst do lewego marginesu";
 $locale['bb_left_usage'] = "Tekst wyrównany do lewego marginesu";
 
