46fcddd62393b9f50807623403edc960dbc4dbaa
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 20:09:36 +0200

    Update articles.php

--- Polish/locale/Polish/search/articles.php
+++ Polish/locale/Polish/search/articles.php
@@ -4,4 +4,4 @@ $locale['a401'] = "Artykuł";
 $locale['a402'] = "Artykuły";
 $locale['a403'] = "znalezione w";
 $locale['a404'] = "Tytuł artykułu";
-$locale['a405'] = "Treć artykułu";
+$locale['a405'] = "Treść artykułu";
