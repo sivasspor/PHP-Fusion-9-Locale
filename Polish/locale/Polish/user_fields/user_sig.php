450848dbb75cd1354fea43ce267875fcc5358912
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 20:31:26 +0200

    Update user_sig.php

--- Polish/locale/Polish/user_fields/user_sig.php
+++ Polish/locale/Polish/user_fields/user_sig.php
@@ -1,6 +1,6 @@
 <?php
-$locale['uf_sig'] = "Signature";
-$locale['uf_sig_desc'] = "Allows extra lines of text.";
-$locale['uf_sig_error'] = "Signature can not be left empty.";
-$locale['na'] = "Signature not available";
+$locale['uf_sig'] = "Podpis";
+$locale['uf_sig_desc'] = "Pozwala na dodatkowe linie tekstu.";
+$locale['uf_sig_error'] = "Podpis nie może być pozostawiony pusty.";
+$locale['na'] = "Podpis niedostępny";
 
