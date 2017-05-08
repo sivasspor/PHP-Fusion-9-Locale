65336f8e2fcc939479226a3bfc4ff3d1d23e24a4
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 20:01:30 +0200

    Update weblinks.php

--- Polish/locale/Polish/permalinks/weblinks.php
+++ Polish/locale/Polish/permalinks/weblinks.php
@@ -1,4 +1,4 @@
 <?php
 $locale['pl_weblinks_title'] = "Linki";
-$locale['pl_weblinks_desc'] = "Włącz bezpośrednie odnośniki do katalogu linków na stronie.";
+$locale['pl_weblinks_desc'] = "Enable Web Links Permalink on the Site.";
 
