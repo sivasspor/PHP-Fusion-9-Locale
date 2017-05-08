735a1ded0fe9ceed93ed8ec0131d0ed4513ef701
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 20:52:45 +0200

    Update submissions.php

--- Polish/locale/Polish/submissions.php
+++ Polish/locale/Polish/submissions.php
@@ -1,8 +1,8 @@
 <?php
 $locale['submit_0000'] = "%s News";
-$locale['submit_0001'] = "%s Articles";
-$locale['submit_0002'] = "%s Downloads";
-$locale['submit_0003'] = "%s Photos";
-$locale['submit_0004'] = "%s Weblinks";
+$locale['submit_0001'] = "%s Artykuł";
+$locale['submit_0002'] = "%s Plik";
+$locale['submit_0003'] = "%s Zdjęcie";
+$locale['submit_0004'] = "%s Link";
 $locale['submit_0005'] = "%s Blog";
-$locale['submit_0006'] = "%s FAQs";
+$locale['submit_0006'] = "%s FAQ";
