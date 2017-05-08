859b0dab0593b0cafb0be84b24e208876a58bded
Eryk <Xyroqs@users.noreply.github.com>
2017-05-08 18:04:33 +0200

    Update news.php

--- Polish/infusions/news/locale/Polish/news.php
+++ Polish/infusions/news/locale/Polish/news.php
@@ -1,5 +1,5 @@
 <?php
-$locale['news_0001'] = "Kontynuuj";
+$locale['news_0001'] = "Kontynuuj czytanie";
 $locale['news_0002'] = "Drukuj";
 $locale['news_0003'] = "Edytuj";
 $locale['news_0004'] = "Aktualności";
@@ -15,3 +15,6 @@ $locale['news_0013'] = "Najczęściej czytane";
 $locale['news_0014'] = "Miniatury";	
 $locale['news_0015'] = "Lista";
 $locale['news_0016'] = "Brak kategorii aktualności.";
+$locale['news_0017'] = "Filtruj według";
+$locale['news_0018'] = "Pokaż wszystkie aktualności";
+$locale['news_0019'] = "Galeria wpisu";
