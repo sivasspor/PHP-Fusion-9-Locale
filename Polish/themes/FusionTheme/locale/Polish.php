3e6424b7db7ecd4678f19176270c11582bf7f90f
Eryk <Xyroqs@users.noreply.github.com>
2017-05-10 21:39:51 +0200

    Update Polish.php

--- Polish/themes/FusionTheme/locale/Polish.php
+++ Polish/themes/FusionTheme/locale/Polish.php
@@ -4,9 +4,9 @@ $locale['NB_000'] = "The Nebula is a PHP-Fusion 9's first FusionTheme Theme Fram
 $locale['NB_001'] = "O nas";
 // Nebula Submissions Template
 $locale['NB_100'] = "Nagłówek wiadomości";
-$locale['NB_101'] = "Write a short and catchy new introduction here.";
-$locale['NB_102'] = "Write the Full News Text Here";
-$locale['NB_103'] = "Submissions Properties";
+$locale['NB_101'] = "Napisz krótkie i chwytliwe wprowadzenie tutaj.";
+$locale['NB_102'] = "Napisz pełną treść newsa tutaj";
+$locale['NB_103'] = "Właściwości zgłoszeń";
 // Nebula News Template
 $locale['NB_200'] = "od";
 // News Module
