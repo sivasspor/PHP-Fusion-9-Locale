1fda533e3c880a41859255a6df75564ca05f2f51
karrak1 <g.sebestyen@fusionjatek.hu>
2017-05-31 22:21:29 +0200

    selectt all localize

--- Hungarian/infusions/weblinks/locale/Hungarian/weblinks_admin.php
+++ Hungarian/infusions/weblinks/locale/Hungarian/weblinks_admin.php
@@ -6,44 +6,31 @@ $locale['WLS_0002'] = "Link Hozzáadása";
 $locale['WLS_0003'] = "Link Szerkesztése";
 $locale['WLS_0004'] = "Kategóriák";
 $locale['WLS_0005'] = "Kategória Hozzáadása";
-$locale['WLS_0006'] = "Kategória Szerkesztése";
 $locale['WLS_0007'] = "Beküldések";
 $locale['WLS_0008'] = "Beállítások";
-
-// Messages - Web Link
 $locale['WLS_0030'] = "Web Link sikeresen Létrehozva";
 $locale['WLS_0031'] = "Web Link sikeresen Módosítva";
 $locale['WLS_0032'] = "Web Link Törölve";
 $locale['WLS_0034'] = "Ki kell választani legalább egy Web Linket";
 $locale['WLS_0035'] = "Web Link sikeresen Publikálva";
 $locale['WLS_0036'] = "Web Link Publikálása Visszavonva";
-
-// Messages - Web Link Cats
 $locale['WLS_0040'] = "Kategória sikeresen Létrehozva";
 $locale['WLS_0041'] = "Kategória sikeresen Módosítva";
 $locale['WLS_0042'] = "Kategória sikeresen Törölve";
 $locale['WLS_0043'] = "A Kategória Nem Törölhető";
 $locale['WLS_0044'] = "Web Linkek vagy alkategóriák vannak ebben a Kategóriában";
-$locale['WLS_0045'] = "Kategóriák sikeresen Módosítva";
 $locale['WLS_0046'] = "A Kategóriák Nem Törölhetőek";
 $locale['WLS_0048'] = "Ki kell választani legalább egy Kategóriát";
 $locale['WLS_0049'] = "Kategóriák sikeresen Publikálva";
 $locale['WLS_0050'] = "Kategóriák Publikálása Törölve";
-
-// Messages - Submissions
 $locale['WLS_0060'] = "Beküldött Web Link Sikeresen Publikálva";
 $locale['WLS_0061'] = "Beküldött Web Link Sikeresen Törölve";
 $locale['WLS_0062'] = "Nincs Beküldött Link";
-$locale['WLS_0063'] = "There are no new Submissions.";
-
-// Table - Web Link
 $locale['WLS_0100'] = "Cím";
 $locale['WLS_0101'] = "Kategória";
 $locale['WLS_0102'] = "Státusz";
 $locale['WLS_0103'] = "Hozzáférés";
 $locale['WLS_0104'] = "Művelet";
-
-// Table - Web Link - Misc
 $locale['WLS_0110'] = "Link Hozzáadása";
 $locale['WLS_0111'] = "Biztosan törölni szeretnéd ezt a linket?";
 $locale['WLS_0112'] = "Nincs Weblink.";
@@ -51,8 +38,6 @@ $locale['WLS_0113'] = "Nincs a Keresési feltételnek megfeleő Web Link.";
 $locale['WLS_0114'] = "Nincs Web Link, vagy Nincs Kategória Létrehozva.";
 $locale['WLS_0115'] = "Web Link ellenőrzés megtörtént, %s hibás linket találtam.";
 $locale['WLS_0116'] = "Státuszát Nem Publikáltra Módosítottam.";
-
-// Table - Web Link - Sorting
 $locale['WLS_0120'] = "Cím";
 $locale['WLS_0121'] = "Keresés";
 $locale['WLS_0122'] = "Törlés";
@@ -64,76 +49,37 @@ $locale['WLS_0127'] = "Összes Kategória";
 $locale['WLS_0128'] = "Válassz Nyelvet";
 $locale['WLS_0129'] = "Összes Nyelv";
 $locale['WLS_0132'] = "Megjelenítés oldalanként";
-$locale['WLS_0133'] = "Neve";
-
-// Table - Web Link Cats
-$locale['WLS_0150'] = "Neve";
 $locale['WLS_0151'] = "Web Link";
-$locale['WLS_0152'] = "Státusz";
-$locale['WLS_0153'] = "Hozzáférés";
-
-// Table - Web Link - Misc
-$locale['WLS_0160'] = "Kategória létrehozása";
 $locale['WLS_0161'] = "Biztosan törölni szeretnéd ezt a Kategóriát?";
 $locale['WLS_0162'] = "Nincs létrehozva Kategória.";
-
-// Table - Submissions
 $locale['WLS_0200'] = "ID";
 $locale['WLS_0201'] = "Link Neve";
 $locale['WLS_0202'] = "Beküldte";
 $locale['WLS_0203'] = "Dátum";
 $locale['WLS_0204'] = "Link Publikálása";
-$locale['WLS_0205'] = "Megnéz"; //"Review Submission";
-
-// Form - Web Link
-$locale['WLS_0250'] = "Link neve";
-$locale['WLS_0251'] = "Oldal neve";
+$locale['WLS_0205'] = "Megnéz";
+$locale['WLS_0206'] = "Összes Kijelölése";
 $locale['WLS_0252'] = "Adj meg egy nevet.";
 $locale['WLS_0253'] = "Web Link URL";
 $locale['WLS_0254'] = "Leírás";
 $locale['WLS_0255'] = "Adj meg egy Leírást";
-$locale['WLS_0256'] = "Kategória";
-$locale['WLS_0257'] = "Hozzáférés";
-$locale['WLS_0258'] = "Dátum";
 $locale['WLS_0259'] = "Közzététel dátumának frissítése";
 $locale['WLS_0260'] = "További Információ";
 $locale['WLS_0261'] = "Link Ellenőrzés";
-
-// Form - Web Link - Errors
 $locale['WLS_0270'] = "Meg kell adni a Leírást.";
-
-// Form - Web Link Cats
 $locale['WLS_0302'] = "Neve";
 $locale['WLS_0303'] = "Főkönyvtár";
-$locale['WLS_0305'] = "Hozzáférés";
-$locale['WLS_0306'] = "Státusz";
-$locale['WLS_0308'] = "További Információk";
-
-// Form - Web Link - Error
 $locale['WLS_0320'] = "Meg kell adni egy Nevet.";
 $locale['WLS_0321'] = "Ilyen nevű Kategória már létezik.";
-$locale['WLS_0322'] = "Meg kell adnia a Leírást.";
-
-// Form - Submissions
 $locale['WLS_0350'] = "Beküldte {%SUBMISSION_AUTHOR%}.";
 $locale['WLS_0351'] = "Beküldve: {%SUBMISSION_TIME%} - {%SUBMISSION_DATE%}.";
-
-// Settings
 $locale['WLS_0400'] = "Web Link Rendszer beállításai.";
-$locale['WLS_0401'] = "Link oldalanként";
-$locale['WLS_0402'] = "Küldhetnek linket";
 $locale['WLS_0403'] = "Kötelező a leírás";
-
-// Submissions - Titles
 $locale['WLS_0900'] = "Link beküldése";
-
-// Submissions - Success Submission
 $locale['WLS_0910'] = "Web Link Sikeresen Beküldve.";
 $locale['WLS_0911'] = "Köszönjük a linket.";
 $locale['WLS_0912'] = "Másik link küldése";
 $locale['WLS_0913'] = "Vissza a [SITENAME]";
-
-// Submissions - Other
 $locale['WLS_0920'] = "Link beküldésére használd az alábbi űrlapot.
 A Link csak akkor kerül ki az oldalra, ha egy Adminisztrátor engedélyezi. [SITENAME] fenntartja a jogot a Link bővítésére, szerkesztésére.
 Csak az oldal tartalmához illeszkedő Link küldj be, különben töröljük.";
