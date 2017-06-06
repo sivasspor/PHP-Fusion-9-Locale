3f422a263c3aca35077a970f4439e56d3b43511e
karrak1 <g.sebestyen@fusionjatek.hu>
2017-06-06 15:06:58 +0200

    locale git

--- Hungarian/locale/Hungarian/reactivate.php
+++ Hungarian/locale/Hungarian/reactivate.php
@@ -3,12 +3,12 @@
 $locale['500'] = "Hiba történt";
 $locale['501'] = "Az aktiváló link, amire rákattintottál, már nem érvényes.<br /><br />Kérjük lépj kapcsolatba oldalunk adminisztrátorával: [SITEEMAIL]";
 $locale['502'] = "Az aktiváló link, amire rákattintottál, nem megfelelő!<br /><br />Kérjük lépj kapcsolatba oldalunk adminisztrátorával: [SITEEMAIL]";
-$locale['503'] = "Az aktiváló link, mire rákattintottál, Nem sikerült újraaktiválni fiókját.\r\n
-Valószínűleg hozzáférésed már aktiválva lett korábban. Ebben az esetben már be kellene tudnod jelentkezni [LINK]ezen az oldalon[/LINK].\r\n
+$locale['503'] = "Az aktiváló linkkel, mire rákattintottál, Nem sikerült újraaktiválni a fiókod.<br />
+Valószínűleg a hozzáférésed már aktiválva lett korábban. Ebben az esetben már be kellene tudnod jelentkezni [LINK]ezen az oldalon[/LINK].<br />
 Ha mégsem tudsz bejelentkezni, kérjük lépj kapcsolatba oldalunk adminisztrátorával: [SITEEMAIL]";
 // Send confirmation mail
 $locale['504'] = "Felhasználó ativálva - [SITENAME]";
-$locale['505'] = "Szia [USER_NAME],\r\n
-Hozzáférésed újraaktiválásra került. Reméljük aktív tagunk leszel a későbbiekben is.\r\n\r\n
-Üdvözlettel,\r\n[SITEUSERNAME]\r\n[SITENAME]";
+$locale['505'] = "Szia [USER_NAME],<br />
+Hozzáférésed újraaktiválásra került. Reméljük aktív tagunk leszel a későbbiekben is.<br />
+Üdvözlettel,<br />[SITEUSERNAME]<br />[SITENAME]";
 $locale['506'] = "Felhasználó által aktiválva.";
