c7f295cf38a55be618de14c2461e9e4e10b78c2e
sivasspor <yusufulku58@hotmail.com>
2017-06-17 19:47:37 +0300

    Update error.php

--- Turkish/locale/Turkish/error.php
+++ Turkish/locale/Turkish/error.php
@@ -1,8 +1,8 @@
 <?php
 $locale['err401'] = "Unauthorized access to this page";
-$locale['err403'] = "Access forbidden to this page";
-$locale['err404'] = "Sorry, this page isn't available on this server";
-$locale['err500'] = "Internal server error";
-$locale['errunk'] = "Unknown error";
+$locale['err403'] = "Bu sayfaya giriş yasaklı";
+$locale['err404'] = "Özür dileriz, bu sayfa sunucuda bulunmamaktadır";
+$locale['err500'] = "İç sunucu hatası";
+$locale['errunk'] = "Bilinmeyen hata";
 $locale['errmsg'] = "The link you followed may have expired, or the page may not be accessible to you.";
-$locale['errret'] = "Return to main site";
+$locale['errret'] = "Ana siteye geri dön";
